3664da84319932204c900408c711c1a1e6666386
iniirsyad <sipaapasipaling@gmail.com>
2025-02-04 21:32:22 +0700

    aku gatau
    ini apaan

---
+++ apa ini/script.js
@@ -0,0 +1,28 @@
+const messages = [
+  "kamu yakin?",
+  "yakin beneran??",
+  "ea tydack bisaa?",
+  "cihuy ...",
+  "semangatt!",
+  "ayo ayo ayo...",
+  "sekali lagi...",
+  "ea...",
+  "sejauh ini lho...",
+  "kamu ngga cape nekan engga terus?",
+  "💖",
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+  const noButton = document.querySelector(".no-button");
+  const yesButton = document.querySelector(".yes-button");
+  noButton.textContent = messages[messageIndex];
+  messageIndex = (messageIndex + 1) % messages.length;
+  const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+  yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+  window.location.href = "yes_page.html";
+}
